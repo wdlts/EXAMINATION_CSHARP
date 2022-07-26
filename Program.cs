cce106bec599a9feb141ce449f30fb9b48a57709
sergeyt <sergej.tokarev@gmail.com>
2022-07-26 12:08:45 +0300

    First method added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.Write("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] CreateStringArray(int size)
+{
+    string[] words = new string[size];
+
+    for (int i = 0; i<size; i=i+1)
+    {
+        Console.Write($"Input word {i+1}: ");
+        words[i] = Console.ReadLine();
+    }
+    Console.WriteLine();
+    return words;
+}
